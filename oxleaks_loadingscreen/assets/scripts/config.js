b43dde3378afd0192b623f3a5259995eb2a66d5c
t2qum <fmslwh77@gmail.com>
2025-05-30 15:16:42 +0300

    Add files via upload

---
+++ oxleaks_loadingscreen/assets/scripts/config.js
@@ -0,0 +1,29 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "localhost:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Everthinghere", icon: "https://cdn.discordapp.com/attachments/1331960920615944315/1377976448421462139/-Photoroom.png?ex=683aec12&is=68399a92&hm=d298ce86cc9b5d0cfec6ac51836b672b9906f8608c23c0f96a7cdf53de3f2d76&", link: "https://discord.gg/mcty"},
+    {name: "tiktok", label: "Tiktok", description: "Sehen Sie sich hier unsere Videos an!", icon: "assets/media/icons/tiktok.png", link: "https://tiktok.com"},
+    {name: "tebex", label: "Tebex", description: "Kaufen Sie unsere Produkte hier!", icon: "assets/media/icons/tebex.png", link: "https://dinero-vip.tebex.io"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Mshari", description: "Server Owner", color: "#fff", image: "https://cdn.discordapp.com/attachments/1375585551284965448/1377770086290423838/18e0974ae7fd3cbd429268b7fb51d5bd.jpg?ex=683ad4a2&is=68398322&hm=51727cdbce74effb47b58b6c611d9a40bf5cf96929d91694c4b19bea0049a2fd&"},
+    {name: "Xdiaa", description: "Owner", color: "#0195FD", image: "https://cdn.discordapp.com/attachments/1375585551284965448/1376707386085081139/00d83df85d5c90d08b73062725bc66e1.gif?ex=683aeb6a&is=683999ea&hm=85c5c6fdada4c581000d11265e24878424d30748c72160d3d552a2e911d7c39b&"},
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
---
+++ oxleaks_loadingscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'highrider#2873'
+description 'DineroCity-Loading'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
